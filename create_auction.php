8d6f181ea18a4d248b4f1fbcc5f40e93d7c3b251
revanpk <oleole999@interia.pl>
2023-11-29 22:43:05 +0100

    code adjustments

--- create_auction.php
+++ create_auction.php
@@ -21,30 +21,35 @@
     <div class="create-auction-container">
         <form action="includes/createAuction.inc.php" class="create-auction-form" method="post">
             <h1>Tworzenie aukcji</h1>
-            <label for="itemName">Nazwa Przedmiotu:</label>
-            <input type="text" name="itemName" id="itemName" required>
+            <div class="first-sector">
+                <div class="column">
+                    <label for="itemName">Nazwa Przedmiotu:</label>
+                    <input type="text" name="itemName" id="itemName" required>
 
-            <label for="category">Kategoria:</label>
-            <select name="category" id="category" required>
-                <?php
-                    foreach ($categories as $category) {
-                        echo "<option value=\"$category\">$category</option>";
-                    }
-                ?>
-            </select>
+                    <label for="category">Kategoria:</label>
+                    <select name="category" id="category" required>
+                        <?php
+                            foreach ($categories as $category) {
+                                echo "<option value=\"$category\">$category</option>";
+                            }
+                        ?>
+                    </select>
+                </div>
+                <div class="column">
+                    <label for="endDate">Planowana Data Zakończenia:</label>
+                    <input type="datetime-local" name="end_date" id="end_date" required>
 
-            <label for="endDate">Planowana Data Zakończenia:</label>
-            <input type="datetime-local" name="end_date" id="end_date" required>
+                    <label for="askingPrice">Cena Wywoławcza:</label>
+                    <input type="number" name="asking_price" id="asking_price" step="0.01" required>
+                </div>
+            </div>
+            <div class="second-sector">
+                <label for="description">Opis:</label>
+                <textarea name="description" id="description" rows="4" required></textarea>
 
-            <label for="askingPrice">Cena Wywoławcza:</label>
-            <input type="number" name="asking_price" id="asking_price" step="0.01" required>
-            
-            <label for="description">Opis:</label>
-            <textarea name="description" id="description" rows="4" required></textarea>
-
-            <label for="picture">Dodaj Zdjęcie:</label>
-            <input type="file" name="picture" id="picture" accept="image/*">
-
+                <label for="picture">Dodaj Zdjęcie:</label>
+                <input type="file" name="picture" id="picture" accept="image/*">
+            </div>
             <button type="submit">Dodaj aukcje</button>
         </form>
     </div>
